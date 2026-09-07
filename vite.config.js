f92520d0f7a6d3aa1f042fd5ec4db0643465f460
eksembl <ejjeejj232@gmail.com>
2026-09-07 21:50:56 +0300

    commit 2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/eksembl.portfolio-github.io/',
   plugins: [
     react(),
     tailwindcss(),
